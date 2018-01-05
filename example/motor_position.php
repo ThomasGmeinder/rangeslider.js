547acf14ce1ef64968ce206935bcf1be7b3815ab
root <thomas@xmos.com>
2018-01-05 08:40:27 +0000

    refactoring and code reduction

--- example/motor_position.php
+++ example/motor_position.php
@@ -16,11 +16,30 @@ $current_mp = -10; // current motor position
 $target_mp = $current_mp;
 $i2c_connected = 1;
 
+// motor event constants
+$NONE = 0;
+$STARTING = 1;
+$STOPPING = 2;
+
+$motor_index = $_POST['motor_index'];
+$regs_per_motor = 4;
+$base_reg = $motor_index * $regs_per_motor;
+
 // Todo: Read server_target_mp it overrides the target_mp from client 
+function read_i2c_reg($reg_idx) {
+    // Read motor target position on Server
+    $command = "i2c_control -r ".$reg_idx;
+    error_log("I2C Read: ".$command);
+    $output = []; // must be re-initialised every time! Otherwise exec keeps adding to it !!
+    exec($command, $output, $return);  
+    if($return != 0) {
+      error_log("Command ".$command." returned with error ".$return);
+    }
+    return (int)$output[0] & 0x7f; // MSB is not valid
+}
 
 if(isset($_POST['target_mp'])) {
   $target_mp = $_POST['target_mp'];
-  $motor_index = $_POST['motor_index'];
   $tc = $_POST['timeout_counter'];
 
   error_log("motor_position.php called with new target position ".$target_mp." for Motor ".$motor_index);
@@ -28,44 +47,29 @@ if(isset($_POST['target_mp'])) {
 
   if($i2c_connected) {
     // Read current motor position
-    $cp_reg = $motor_index*4 + 2;
-    $command = "i2c_control -r ".$cp_reg;
-    error_log($command);
-    $output = []; // must be re-initialised every time! Otherwise exec keeps adding to it !!
-    exec($command, $output, $return);  
-    $current_mp = (int)$output[0] & 0x7f; // MSB not valid
+    $cp_reg = $base_reg + 2;
+    $current_mp = read_i2c_reg($cp_reg);
     error_log("Current Position of Motor ".$motor_index." read via I2C is ".$current_mp);
 
     // Read motor target position on Server
-    $tp_reg = $motor_index*4 + 1;
-    $command = "i2c_control -r ".$tp_reg;
-    error_log($command);
-    $output = []; 
-    exec($command, $output, $return);  
-    if($return != 0) {
-      error_log("Command ".$command." returned with error ".$return);
-    }
-    $server_target_mp = (int)$output[0] & 0x7f; // MSB not valid
+    $tp_reg = $base_reg + 1;
+    $server_target_mp = read_i2c_reg($tp_reg); 
     error_log("Server Target Position of Motor ".$motor_index." read via I2C is ".$server_target_mp);
 
     // Read current motor position
-    $status_reg = $motor_index*4 + 3;
-    $command = "i2c_control -r ".$status_reg;
-    $output = []; 
-    exec($command, $output, $return);  
-    error_log("command ".$command.", exec output ".(int)$output[0].", return ".$return);
-    $status = (int)$output[0] & 0x7f; // MSB not valid
+    $trigger_reg = $base_reg + 3;
+    $trigger = read_i2c_reg($trigger_reg);
 
-    if($status != 0) { 
-    //if($server_target_mp != $current_mp) {
+    if($trigger != 0) { 
       // Button control on server takes priority
       // Update target_mp as feedback to client
-      error_log("status == ".$status);
+      error_log("trigger == ".$trigger);
       error_log("Target position of Motor ".$motor_index." was changed on server with Buttons. Updating target_mp to ".$server_target_mp." for client to update the sliders");
       $target_mp = $server_target_mp;
     }
     else if($current_mp != $target_mp) {
       // set new target motor position via I2C
+      // Todo: only do this once
       $command = "i2c_control -w ".$tp_reg." ".$target_mp;
       system($command);
   
@@ -75,10 +79,12 @@ if(isset($_POST['target_mp'])) {
 //        error_log("Starting Motor ".$motor_index." in closing direction from ".$current_mp);
         $ctrl_val = 1;
       }
-      $state_reg = $motor_index*4; 
+      $state_reg = $base_reg; 
       $command = "i2c_control -w ".$state_reg." ".$ctrl_val;
       system($command);
     } 
+
+
   } else {
     // just for testing:
     if($tc > 5) {
@@ -99,7 +105,7 @@ if(isset($_POST['target_mp'])) {
   $motor_index = $_POST['motor_index'];
   if($i2c_connected) { 
     // Read current motor position
-    $cp_reg = $motor_index*4 + 2;
+    $cp_reg = $base_reg + 2;
     error_log($cp_reg);
     $command = "i2c_control -r ".$cp_reg;
     exec($command, $output, $return);  
